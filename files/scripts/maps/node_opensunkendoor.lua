9a90a0fe6659e98c7e802d94a1ed4ea12b825f26
Louis M <prog@tioui.com>
2024-07-14 12:44:57 -0400

    Makes sunken city door openable once Li has been capture

--- files/scripts/maps/node_opensunkendoor.lua
+++ files/scripts/maps/node_opensunkendoor.lua
@@ -29,6 +29,8 @@ function init(me)
 	
 	if hasLi() then
 		node_setCursorActivation(me, true)
+	elseif getFlag(FLAG_SUNKENCITY_PUZZLE) >= SUNKENCITY_BOSSDONE then
+		node_setCursorActivation(me, true)
 	end
 	
 	loadSound("sunkendoor-unlock")
@@ -46,66 +48,73 @@ function update(me, dt)
 end
 
 function activate(me)
-	local li = getLi()
-	local n = getNaija()
-	
-	v.doflip = true
-	
-	local fadet = 0.5
-	
-	local wrench = entity_getBoneByName(li, "wrench")
-	
-	entity_setState(li, STATE_OPEN)
-	
-	
-	
-	if entity_isfh(n) then entity_fh(n) end
-	
-	overrideZoom(1.2, 1)
-	
-	fade2(1, fadet) watch(fadet)
-	
-	
-	entity_setPosition(li, node_x(v.linode), node_y(v.linode))
-	
-	cam_toEntity(li)
-	
-	entity_setPosition(n, node_x(v.linode)+64, node_y(v.linode)-15)
-	bone_setVisible(wrench, 1)
-	entity_animate(li, "work", 0, 4)
-	
-	if entity_isfh(li) then entity_fh(li) end
-	
-	fade2(0, fadet) watch(fadet)
-	
-	watch(3)
-	
-	--- click
-	playSfx("sunkendoor-unlock")
-	
-	entity_msg(li, "expression", EXPRESSION_SURPRISE)
-	
-	watch(0.8)
-	
-	entity_msg(li, "expression", EXPRESSION_HAPPY)
-	
-	playSfx("sunkendoor-open")
-	entity_setState(v.door, STATE_OPEN)
-	
-	overrideZoom(0.7, 4)
-	
-	watch(1.8)
-	entity_animate(li, "idle", -1)
-	bone_scale(wrench, 0, 0, 1)
-	
-	voiceOnce("Naija_SunkenCityOpened")
-	
-	entity_setState(li, STATE_IDLE)
-	cam_toEntity(n)
-	
-	node_setCursorActivation(me, false)
-	v.doflip = false
-	
-	overrideZoom(0)
+	if hasLi() then
+		 	local li = getLi()
+		local n = getNaija()
+		
+		v.doflip = true
+		
+		local fadet = 0.5
+		
+		local wrench = entity_getBoneByName(li, "wrench")
+		
+		entity_setState(li, STATE_OPEN)
+		
+		
+		
+		if entity_isfh(n) then entity_fh(n) end
+		
+		overrideZoom(1.2, 1)
+		
+		fade2(1, fadet) watch(fadet)
+		
+		
+		entity_setPosition(li, node_x(v.linode), node_y(v.linode))
+		
+		cam_toEntity(li)
+		
+		entity_setPosition(n, node_x(v.linode)+64, node_y(v.linode)-15)
+		bone_setVisible(wrench, 1)
+		entity_animate(li, "work", 0, 4)
+		
+		if entity_isfh(li) then entity_fh(li) end
+		
+		fade2(0, fadet) watch(fadet)
+		
+		watch(3)
+		
+		--- click
+		playSfx("sunkendoor-unlock")
+		
+		entity_msg(li, "expression", EXPRESSION_SURPRISE)
+		
+		watch(0.8)
+		
+		entity_msg(li, "expression", EXPRESSION_HAPPY)
+		
+		playSfx("sunkendoor-open")
+		entity_setState(v.door, STATE_OPEN)
+		
+		overrideZoom(0.7, 4)
+		
+		watch(1.8)
+		entity_animate(li, "idle", -1)
+		bone_scale(wrench, 0, 0, 1)
+		
+		voiceOnce("Naija_SunkenCityOpened")
+		
+		entity_setState(li, STATE_IDLE)
+		cam_toEntity(n)
+		
+		node_setCursorActivation(me, false)
+		v.doflip = false
+		
+		overrideZoom(0)
+	else
+
+		playSfx("sunkendoor-open")
+		entity_setState(v.door, STATE_OPEN)
+		node_setCursorActivation(me, false)
+	end
 end
 
