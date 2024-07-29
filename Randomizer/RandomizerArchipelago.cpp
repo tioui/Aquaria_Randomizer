ea5af811229ac009db86fb625d7a311c113d7982
Louis M <prog@tioui.com>
2024-07-29 14:28:44 -0400

    Message filtering did not work with aliases

--- Randomizer/RandomizerArchipelago.cpp
+++ Randomizer/RandomizerArchipelago.cpp
@@ -278,7 +278,7 @@ void RandomizerArchipelago::onPrintJson (const APClient::PrintJSONArgs& aJson){
 bool RandomizerArchipelago::selfRelatedJson(const std::list<APClient::TextNode>& aData) {
     bool lResult = false;
     for (const APClient::TextNode& lNode : aData) {
-        if (lNode.type == "player_id" and apClient->get_player_alias(std::stoi(lNode.text)) == name) {
+        if (lNode.type == "player_id" and apClient->get_player_number() == std::stoi(lNode.text)) {
             lResult = true;
         };
     }
