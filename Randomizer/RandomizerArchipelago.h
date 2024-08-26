311e95eca9f9be108dbd7bbbe380112b8074fac5
Louis M <prog@tioui.com>
2024-08-26 19:04:51 -0400

    Changing RandomizerArchipelago.h to adapt to 9599e38

--- Randomizer/RandomizerArchipelago.h
+++ Randomizer/RandomizerArchipelago.h
@@ -357,6 +357,12 @@ private:
     void loadConnectionInfo();
 
     /**
+     * A reply to an AP location scout message
+     * @param aItems Every items that has been scout
+     */
+    void onLocationInfo(const std::list<APClient::NetworkItem>& aItems);
+
+    /**
      * True if the player is playing offline
      */
     bool isOffline;
