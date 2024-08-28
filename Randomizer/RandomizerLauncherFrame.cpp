51b0b6d25f78f63fe588155c98f8b654a9133de2
Louis M <prog@tioui.com>
2024-08-28 17:36:56 -0400

    Filter new save directory format in offline launcher tab

--- Randomizer/RandomizerLauncherFrame.cpp
+++ Randomizer/RandomizerLauncherFrame.cpp
@@ -324,7 +324,8 @@ wxWindow *RandomizerLauncherFrame::buildArchipelagoOfflinePanel(wxWindow *aParen
  */
 void RandomizerLauncherFrame::fillSeedNumber(wxArrayString *aChoice) {
     for(auto& p : std::filesystem::recursive_directory_iterator(userDataFolder)) {
-        if (p.is_directory() && strncmp(p.path().filename().string().c_str(), "save_", 5) == 0) {
+        if ((p.is_directory() && strncmp(p.path().filename().string().c_str(), "save_", 5) == 0) &&
+            (p.path().filename().string().find("_", 5) != std::string::npos)) {
             aChoice->push_back(p.path().filename().string().substr(5, std::string::npos));
         }
     }
